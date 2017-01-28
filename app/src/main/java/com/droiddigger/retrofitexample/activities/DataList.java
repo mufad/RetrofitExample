ec878af2cba80d1a355a48f4a20d7acf5fa533e4
Monwar Jahan <mmufad@gmail.com>
2017-01-28 21:48:24 +0600

    DataList activity added

---
+++ app/src/main/java/com/droiddigger/retrofitexample/activities/DataList.java
@@ -0,0 +1,15 @@
+package com.droiddigger.retrofitexample.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.droiddigger.retrofitexample.R;
+
+public class DataList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_list);
+    }
+}
